9dcf2cdd26418b87d8d293075528149fd9e3e05b
shmawilton <246wiltononyango@gmail.com>
2023-06-16 07:59:30 +0300

    second commit

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,15 +3,13 @@ import logo from '../assets/light-bulb.svg'
 
 const Header = () => {
   return (
-    <div>
+    <>
       <Image src={logo} alt='logo' width={100} marginBottom='1rem'/>
       <Heading color='white' marginBottom='1rem'>AI Keyword Extractor</Heading>
         <Text fontSize={25} textAlign='center'>
             Paste in your text below and well extract the keywords for you
-        </Text>
-
-    
-    </div>
+        </Text>    
+    </>
   )
 }
 
